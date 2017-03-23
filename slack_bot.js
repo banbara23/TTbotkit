4805a22586376bd8bf754d5ae278f71acc9d3502
池村 和剛 <kazutakai@tt-com.net>
2017-03-24 00:08:46 +0900

    shファイル名間違い

--- slack_bot.js
+++ slack_bot.js
@@ -143,7 +143,7 @@ function updateSelf(bot, message) {
 // 撮影
 controller.hears(['撮影', 'webcam'], 'ambient', function(bot, message) {
     // bot.reply(message, '撮影開始');
-    child_process.exec('/home/pi/webcam.sh', function(error, stdout, stderr) {
+    child_process.exec('/home/pi/webcamSlack.sh', function(error, stdout, stderr) {
         if (error) {
             bot.reply('fswebcamエラー');
             bot.reply(message, stderr);
